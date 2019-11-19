6b5a6a0320cf523acdec44005a76904d918ba626
danobilo <danobilo@gmail.com>
2019-11-19 15:45:01 +0300

    Added getter and setter for the name field

--- src/org/danobilo/model/User.java
+++ src/org/danobilo/model/User.java
@@ -3,4 +3,14 @@ package org.danobilo.model;
 public class User {
 
     private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+    
+    
 }
